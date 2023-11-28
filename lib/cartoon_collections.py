7c1423e5c91a2a7aa8aff946a1ddca4603fdda40
root <root@DESKTOP-UEO62DE.localdomain>
2023-11-28 13:46:11 +0300

    Complete cartoon collections

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for position, dwarf in enumerate (dwarves, start = 1):
+        print (f"{position}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteers):
+    new_list = [f"{planeteer.title()}!" for planeteer in planeteers]
+    return new_list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
+def find_the_cheese(cheeses):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+
+    for cheese in cheeses:
+        if cheese in cheese_types:
+            return cheese
+        
+    return None
+
+
+
     pass
